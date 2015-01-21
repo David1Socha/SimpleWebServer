6decb61d798440b4deb565ac0a139eb3aaf4d616
David Socha <David1Socha@gmail.com>
2015-01-21 01:54:50 -0600

    Add ExecuteCode method to CSharpServerCodeExecutor

--- WebServer/CSharpServerCodeExecutor.cs
+++ WebServer/CSharpServerCodeExecutor.cs
@@ -2,7 +2,9 @@
 using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -35,17 +37,46 @@ namespace WebServer
             _params.CompilerOptions = "/t:library";
         }
 
-        public static ScriptResult executeCode(String code)
+        public ScriptResult ExecuteCode(String code, IDictionary<string, string> requestParameters)
         {
-
+            ScriptResult scriptResult;
+            String classString = _BuildClassString(code);
+            CompilerResults result = _provider.CompileAssemblyFromSource(_params, classString);
+            bool hasCompilationErrors = result.Errors.Count > 0;
+            if (hasCompilationErrors)
+            {
+                scriptResult = _BuildCompilationErrorResponse(result.Errors);
+            }
+            else
+            {
+                Assembly codeAssembly = result.CompiledAssembly;
+                object instance = codeAssembly.CreateInstance("Server.Executor");
+                Type instanceType = instance.GetType();
+                MethodInfo executionMethod = instanceType.GetMethod("Execute", new Type[] { typeof(StringWriter), typeof(Dictionary<string, string>) });
+                try
+                {
+                    StringWriter sw = new StringWriter();
+                    executionMethod.Invoke(instance, new object[] { sw, requestParameters });
+                    scriptResult = new ScriptResult()
+                    {
+                        Result = sw.ToString(),
+                        Error = false
+                    };
+                }
+                catch (Exception e)
+                {
+                    scriptResult = _BuildRuntimeErrorResponse(e.InnerException);
+                }
+            }
+            return scriptResult;
         }
 
-        private static String _buildClassString(String code)
+        private static String _BuildClassString(String code)
         {
             return String.Format(_classTemplate, code);
         }
 
-        private static ScriptResult _buildCompilationErrorResponse(CompilerErrorCollection compilerErrors)
+        private static ScriptResult _BuildCompilationErrorResponse(CompilerErrorCollection compilerErrors)
         {
             StringBuilder response = new StringBuilder();
             response.Append(_compilationErrorHeader);
@@ -62,7 +93,7 @@ namespace WebServer
             };
         }
 
-        private static ScriptResult _buildRuntimeErrorResponse(Exception runtimeException)
+        private static ScriptResult _BuildRuntimeErrorResponse(Exception runtimeException)
         {
             String response = String.Format(_runtimeErrorMessage, runtimeException.Message);
             return new ScriptResult()
